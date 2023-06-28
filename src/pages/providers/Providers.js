164e0ddb6e61b5c1cec544ba1e974034d0db2fd0
Radvan Alexandru <rip777ton@gmail.com>
2023-06-28 20:37:54 +0300

    Add the table on the providers page

--- src/pages/providers/Providers.js
+++ src/pages/providers/Providers.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Providers.css";
 import useFetch from "../../hooks/useFetch";
+import Table from "../../components/Table/Table";
 const Providers = () => {
   const { data, loading, error } = useFetch(
     "http://localhost:8080/api/provider/readAll"
   );
+  const [providers, setProviders] = useState(null);
   useEffect(() => {
     console.log(data);
+    if (data) {
+      const newArray = data.map((provider) => {
+        return {
+          id: provider.id,
+          providerName: provider.providerName,
+          email: provider.email,
+          ["Nr. of positions"]: provider.positions.length,
+        };
+      });
+      setProviders(newArray);
+    }
   }, [data]);
-  return <div className="Providers">Providers</div>;
+  return (
+    <div className="Providers">
+      {providers && <Table data={providers} page="providers" />}
+    </div>
+  );
 };
 
 export default Providers;
