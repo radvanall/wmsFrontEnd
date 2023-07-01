d2eb74cb504b1367aba9c5d7ca9708b1f972d411
Radvan Alexandru <rip777ton@gmail.com>
2023-07-01 22:26:02 +0300

    Add the SingleProvider element to the SingleProvider page

--- src/pages/SingleProvider/SingleProvider.js
+++ src/pages/SingleProvider/SingleProvider.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ProviderCard from "../../components/ProviderCard/ProviderCard";
 import { useParams } from "react-router-dom";
+import useGetData from "../../hooks/useGetData";
 const SingleProvider = () => {
   const { providerId } = useParams();
+  const { data, loading, error, getData } = useGetData(
+    "http://localhost:8080/api/provider/read/"
+  );
+
   console.log(providerId);
-  return <div>SingleProvider</div>;
+  useEffect(() => {
+    getData(providerId);
+    console.log(data);
+  }, []);
+  return (
+    <div>
+      SingleProvider
+      {data && (
+        <div style={{ maxWidth: 300 }}>
+          <ProviderCard provider={data} />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SingleProvider;
