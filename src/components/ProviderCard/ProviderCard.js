13dbbcd89f1bec32664c549c254fd1bcb1519cd3
Radvan Alexandru <rip777ton@gmail.com>
2023-07-02 22:59:13 +0300

    Add the EditProvider element to the ProviderCard and create buttons for edit and delete

--- src/components/ProviderCard/ProviderCard.js
+++ src/components/ProviderCard/ProviderCard.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
+import { TiEdit } from "react-icons/ti";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { useToggle } from "../../hooks/useToggle";
+import EditProvider from "../EditProvider/EditProvider";
 import Card from "../Card/Card";
 import "./ProviderCard.css";
 
-const ProviderCard = ({ provider }) => {
+const ProviderCard = ({ provider, getData }) => {
+  const { status: isOpenModify, toggleStatus: toggleModify } = useToggle(false);
+  const { status: isOpenDelete, toggleStatus: toggleDelete } = useToggle(false);
   useEffect(() => {
     console.log(provider);
   }, [provider]);
+  const fetchData = () => {
+    getData(provider.id);
+  };
   return (
     <Card>
       <div className="provider__card__wrapper">
@@ -32,7 +41,32 @@ const ProviderCard = ({ provider }) => {
             <span>{provider.positions.length ?? "0"}</span>
           </p>
         </div>
+
+        <div className="provider__actions">
+          <TiEdit
+            className={
+              isOpenModify
+                ? "search_menu_button menu__opened"
+                : "search_menu_button menu__closed"
+            }
+            onClick={toggleModify}
+          />
+          <RiDeleteBin6Line
+            className={
+              isOpenDelete
+                ? "search_menu_button menu__opened"
+                : "search_menu_button menu__closed"
+            }
+            onClick={toggleDelete}
+          />
+        </div>
       </div>
+      <EditProvider
+        provider={provider}
+        active={isOpenModify}
+        setActive={toggleModify}
+        fetchData={fetchData}
+      />
     </Card>
   );
 };
