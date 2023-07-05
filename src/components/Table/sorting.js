3a1ac53e8fb3e267c19ac22b815d334c79e22f7c
Radvan Alexandru <rip777ton@gmail.com>
2023-07-05 23:41:47 +0300

    Add a new if statament that verifies if the column is an object and in case that it is an object, sorts the rows by the text field of the object

--- src/components/Table/sorting.js
+++ src/components/Table/sorting.js
@@ -26,6 +26,26 @@ const sorting = (
     }
     return;
   }
+  if (typeof data[0][col] === "object") {
+    console.log("object");
+    if (order === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].text.toLowerCase() < b[col].text.toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("DSC");
+      setSortColumnsState({ ...sortColumnsState, [col]: "DSC" });
+    }
+    if (order === "DSC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].text.toLowerCase() > b[col].text.toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("ASC");
+      setSortColumnsState({ ...sortColumnsState, [col]: "ASC" });
+    }
+    return;
+  }
   if (order === "ASC") {
     const sorted = [...data].sort((a, b) => a[col] - b[col]);
     setOrder("DSC");
