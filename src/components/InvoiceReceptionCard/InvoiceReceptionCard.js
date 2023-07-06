e0c62323cc0d0226ffa52ea297db5db2b15e4f69
Radvan Alexandru <rip777ton@gmail.com>
2023-07-06 23:30:08 +0300

    Create a InvoiceReceptionCard and add it to SingleInvoice page

---
+++ src/components/InvoiceReceptionCard/InvoiceReceptionCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Card/Card";
+const InvoiceReceptionCard = ({ invoice }) => {
+  console.log("invoice:", invoice);
+  return (
+    <div>
+      <Card>InvoiceReceptionCard</Card>
+    </div>
+  );
+};
+
+export default InvoiceReceptionCard;
---
+++ src/pages/SingleInvoice/SingleInvoice.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useGetData from "../../hooks/useGetData";
+import InvoiceReceptionCard from "../../components/InvoiceReceptionCard/InvoiceReceptionCard";
+const SingleInvoice = () => {
+  const { invoiceId } = useParams();
+  const [invoice, setInvoice] = useState();
+  const { data, loading, error, getData } = useGetData(
+    "http://localhost:8080/api/invoiceReception/read/"
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      await getData(invoiceId);
+    }
+    fetchData();
+  }, [invoiceId]);
+
+  useEffect(() => {
+    if (data) {
+      const { stocks, ...newObject } = data;
+      setInvoice(newObject);
+    }
+  }, [data]);
+
+  console.log("invoiceId:", invoiceId);
+  return (
+    <div>
+      SingleInvoice
+      <InvoiceReceptionCard invoice={invoice} />{" "}
+    </div>
+  );
+};
+
+export default SingleInvoice;
