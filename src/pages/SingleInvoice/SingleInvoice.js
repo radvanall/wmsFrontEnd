1e379b98a01d63478195271a5983b7ae4d763506
Radvan Alexandru <rip777ton@gmail.com>
2023-07-17 17:03:27 +0300

    Add AddStock modal to the SingleInvoice.js page

--- src/pages/SingleInvoice/SingleInvoice.js
+++ src/pages/SingleInvoice/SingleInvoice.js
@@ -10,6 +10,7 @@ import Card from "../../components/Card/Card";
 import usePostData from "../../hooks/usePostData";
 import { BiPlusMedical } from "react-icons/bi";
 import { useToggle } from "../../hooks/useToggle";
+import AddStock from "../../components/AddStock/AddStock";
 
 const SingleInvoice = () => {
   const { invoiceId } = useParams();
@@ -17,6 +18,7 @@ const SingleInvoice = () => {
   const { status: isOpenEdit, toggleStatus: toggleEdit } = useToggle(false);
   const [invoice, setInvoice] = useState();
   const [stocks, setStocks] = useState();
+  const [selectedStock, setSelectedStock] = useState();
   const { data, loading, error, getData } = useGetData(
     "http://localhost:8080/api/invoiceReception/read/"
   );
@@ -27,6 +29,7 @@ const SingleInvoice = () => {
     resetMessage,
     postData,
   } = usePostData();
+  const refetch = () => getData(invoiceId);
 
   useEffect(() => {
     async function fetchData() {
@@ -56,6 +59,11 @@ const SingleInvoice = () => {
     await postData({ id: id }, `http://localhost:8080/api/stock/delete`);
     getData(invoiceId);
   };
+  const openEdit = (id) => {
+    const stock = stocks.find((item) => item.id === id);
+    setSelectedStock(stock);
+    toggleEdit();
+  };
   console.log("invoiceId:", invoiceId);
   return (
     <div className="single__invoice">
@@ -72,7 +80,7 @@ const SingleInvoice = () => {
                     ? "search_menu_button menu__opened"
                     : "search_menu_button menu__closed"
                 }
-                // onClick={toggleCreate}
+                onClick={toggleCreate}
               />
             </div>
           )}
@@ -80,19 +88,30 @@ const SingleInvoice = () => {
           <ResponsiveTable
             data={stocks}
             title="Stocuri:"
-            handleEdit={
-              invoice.validated
-                ? null
-                : (id) => {
-                    console.log(id);
-                    toggleEdit();
-                  }
-            }
+            handleEdit={invoice.validated ? null : openEdit}
             handleDelete={invoice.validated ? null : deleteStock}
           />
         </Card>
       )}
-      <EditStock active={isOpenEdit} handleCloseModal={toggleEdit} />
+      {stocks && selectedStock && (
+        <EditStock
+          active={isOpenEdit}
+          handleCloseModal={toggleEdit}
+          stock={selectedStock}
+          refetch={refetch}
+        />
+      )}
+      {invoice && (
+        <AddStock
+          active={isOpenCreate}
+          handleCloseModal={toggleCreate}
+          image={invoice.image}
+          provider={invoice.provider}
+          providerId={invoice.providerId}
+          invoiceId={invoice.id}
+          refetch={refetch}
+        />
+      )}
     </div>
   );
 };
