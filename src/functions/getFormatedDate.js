bc69fb81095f1a2bb00667bb148dc2a7b5616f33
Radvan Alexandru <rip777ton@gmail.com>
2023-07-21 18:45:26 +0300

    Add a return '--' in case if the recived argument is null.

--- src/functions/getFormatedDate.js
+++ src/functions/getFormatedDate.js
@@ -1,5 +1,6 @@
 const getFormatedDate = (date, locale) => {
   const options = { day: "numeric", month: "long", year: "numeric" };
+  if (!date) return "--";
   return new Date(date).toLocaleDateString(locale, options).toUpperCase();
 };
 export default getFormatedDate;
