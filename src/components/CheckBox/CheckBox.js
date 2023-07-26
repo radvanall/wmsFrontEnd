85f319c97918341c1458edca2e9a8a41ec9b0646
Radvan Alexandru <rip777ton@gmail.com>
2023-07-26 15:37:15 +0300

    Add new propd to CheckBox component that controls the value and the style of the element. Add new styles to Checkbox.

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,11 +1,39 @@
+import { borderRadius, padding } from "@mui/system";
 import React from "react";
 import "./CheckBox.css";
-const CheckBox = ({ id, name, label, border }) => {
+const CheckBox = ({
+  id,
+  name,
+  label,
+  border,
+  value,
+  checked,
+  attribute,
+  handleChange,
+  padding,
+  borderRadius,
+  backgroundColor,
+  width,
+}) => {
   return (
     <div
+      style={{
+        backgroundColor,
+        padding,
+        borderRadius,
+        width,
+      }}
       className={border ? "checkbox__container border" : "checkbox__container"}
     >
-      <input type="checkbox" id={id} name={name} />
+      <input
+        type="checkbox"
+        id={id}
+        name={name}
+        value={value}
+        checked={checked}
+        data-custom-attribute={attribute}
+        onChange={handleChange}
+      />
       <label htmlFor={id}>{label}</label>
     </div>
   );
