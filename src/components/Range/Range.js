cedbdebb7d28aec41480cb096c3f09a5e6661070
Radvan Alexandru <rip777ton@gmail.com>
2023-07-26 15:39:23 +0300

    Remove the width propriety from label and range__wrapper in Range.css. Add new props to the Range.js that allow the component to get width values for label and input from outside.

--- src/components/Range/Range.js
+++ src/components/Range/Range.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./Range.css";
-const Range = ({ name, id, value, min, max, unit, handleChange }) => {
+const Range = ({
+  name,
+  id,
+  value,
+  min,
+  max,
+  unit,
+  handleChange,
+  labelWidth = "30%",
+  rangeWidth = "65%",
+}) => {
   return (
     <div className="range__container">
-      <div className="range__wrapper">
+      <div className="range__wrapper" style={{ width: rangeWidth }}>
         <input
           type="range"
           name={name}
@@ -15,7 +25,7 @@ const Range = ({ name, id, value, min, max, unit, handleChange }) => {
         />
         <progress value={value} max={max ?? 10000} />
       </div>
-      <label>{value + " " + unit}</label>
+      <label style={{ width: labelWidth }}>{value + " " + unit}</label>
     </div>
   );
 };
