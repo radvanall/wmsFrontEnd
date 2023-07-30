4e63fbe8e49d418ee046532919415cbcc90d4824
Radvan Alexandru <rip777ton@gmail.com>
2023-07-30 12:19:53 +0300

    Add checkboxes for  the products , providers , categories ,subcategories and status. If the arrays that hold the data for this checkboxes are empty the tagHolder will not be displayed.

--- src/components/TagHolder/TagHolder.js
+++ src/components/TagHolder/TagHolder.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   handleCheckboxChange,
   setFilterCriterias,
+  changeStatus,
 } from "../../toolkitRedux/stockFilterSlice";
 const TagHolder = ({ data, filterCriterias, filterStocks }) => {
   const dispatch = useDispatch();
@@ -19,12 +20,28 @@ const TagHolder = ({ data, filterCriterias, filterStocks }) => {
   );
   const status = useSelector((state) => state.stockFilterSlice.status);
 
+  const providers = displayedValues.provider?.filter((item) =>
+    filterCriterias.providers.some((id) => parseInt(item.id) === id)
+  );
+  const categories = displayedValues.category?.filter((item) =>
+    filterCriterias.categories.some((id) => parseInt(item.id) === id)
+  );
+  const subcategories = displayedValues.subcategory?.filter((item) =>
+    filterCriterias.subcategories.some((id) => parseInt(item.id) === id)
+  );
+  const products = displayedValues.product?.filter((item) =>
+    filterCriterias.products.some((id) => parseInt(item.id) === id)
+  );
+  console.log("tagProducts:", products);
+  const states = Object.keys(status).filter(
+    (key) => status[key] === true && key !== "allStates"
+  );
+
   const getCriteriasIds = (criteria) => {
     return checkboxStates[criteria]
       .filter((item) => item.checked)
       .map((item) => item.id);
   };
-
   const handleFilter = (attribute, id) => {
     const checkedStatus = Object.keys(status).filter((item) => status[item]);
     console.log("chekedStatus:", checkedStatus);
@@ -41,11 +58,15 @@ const TagHolder = ({ data, filterCriterias, filterStocks }) => {
       maxQuantity: rangeValues.maxQuantity,
       minQuantity: rangeValues.minQuantity,
     };
+
     const filteredCurrentfilterCriterias = {
       ...currentfilterCriterias,
-      [attribute]: currentfilterCriterias[attribute].filter(
-        (item) => parseInt(item) !== parseInt(id)
-      ),
+      [attribute]:
+        attribute === "status"
+          ? checkedStatus.filter((item) => item !== id)
+          : currentfilterCriterias[attribute].filter(
+              (item) => parseInt(item) !== parseInt(id)
+            ),
     };
     dispatch(setFilterCriterias());
     filterStocks(filteredCurrentfilterCriterias);
@@ -66,91 +87,122 @@ const TagHolder = ({ data, filterCriterias, filterStocks }) => {
     );
     handleFilter(attribute, e.target.value);
   };
+  const handleStatusChange = (e, attribute) => {
+    dispatch(
+      changeStatus({ value: e.target.value, checked: e.target.checked })
+    );
+    console.log("attribute=", attribute);
+    console.log("value=", e.target.value);
+    handleFilter(attribute, e.target.value);
+  };
   return (
-    <div className="tag__holder">
-      <h2 className="tag__holder__name">Tags</h2>
-      {displayedValues.provider
-        ?.filter((item) =>
-          filterCriterias.providers.some((id) => parseInt(item.id) === id)
-        )
-        .map((provider) => (
-          <CheckBox
-            id={"provider" + provider.id}
-            key={"provider" + provider.id}
-            value={provider.id}
-            name={provider.providerName}
-            label={provider.providerName}
-            attribute="provider"
-            border={true}
-            checked={
-              checkboxStates?.provider?.find((item) => item.id === provider.id)
-                ?.checked || false
-            }
-            handleChange={(e) => handleChange(e, "providers")}
-          />
-        ))}
-      {displayedValues.category
-        ?.filter((item) =>
-          filterCriterias.categories.some((id) => parseInt(item.id) === id)
-        )
-        .map((category) => (
-          <CheckBox
-            id={"category" + category.id}
-            key={"category" + category.id}
-            value={category.id}
-            name={category.categoryName}
-            label={category.categoryName}
-            attribute="category"
-            border={true}
-            checked={
-              checkboxStates?.category?.find((item) => item.id === category.id)
-                ?.checked || false
-            }
-            handleChange={(e) => handleChange(e, "categories")}
-          />
-        ))}
-      {displayedValues.subcategory
-        ?.filter((item) =>
-          filterCriterias.subcategories.some((id) => parseInt(item.id) === id)
-        )
-        .map((subcategory) => (
-          <CheckBox
-            id={"subcategory" + subcategory.id}
-            key={"subcategory" + subcategory.id}
-            value={subcategory.id}
-            name={subcategory.subcategoryName}
-            label={subcategory.subcategoryName}
-            attribute="subcategory"
-            border={true}
-            checked={
-              checkboxStates?.subcategory?.find(
-                (item) => item.id === subcategory.id
-              )?.checked || false
-            }
-            handleChange={(e) => handleChange(e, "subcategories")}
-          />
-        ))}
-      {displayedValues.product
-        ?.filter((item) =>
-          filterCriterias.products.some((id) => parseInt(item.id) === id)
-        )
-        .map((product) => (
-          <CheckBox
-            id={"product" + product.id}
-            key={"product" + product.id}
-            value={product.id}
-            name={product.productName}
-            label={product.productName}
-            attribute="product"
-            border={true}
-            checked={
-              checkboxStates?.product?.find((item) => item.id === product.id)
-                ?.checked || false
-            }
-            handleChange={(e) => handleChange(e, "products")}
-          />
-        ))}
-    </div>
+    <>
+      {states.length ||
+      products.length ||
+      categories.length ||
+      subcategories.length ||
+      providers.length ? (
+        <div className="tag__holder">
+          <h2 className="tag__holder__name">Taguri:</h2>
+
+          {providers.map((provider) => (
+            <CheckBox
+              id={"provider" + provider.id}
+              key={"provider" + provider.id}
+              value={provider.id}
+              name={provider.providerName}
+              label={provider.providerName}
+              attribute="provider"
+              border={true}
+              checked={
+                checkboxStates?.provider?.find(
+                  (item) => item.id === provider.id
+                )?.checked || false
+              }
+              handleChange={(e) => handleChange(e, "providers")}
+            />
+          ))}
+
+          {categories.map((category) => (
+            <CheckBox
+              id={"category" + category.id}
+              key={"category" + category.id}
+              value={category.id}
+              name={category.categoryName}
+              label={category.categoryName}
+              attribute="category"
+              border={true}
+              checked={
+                checkboxStates?.category?.find(
+                  (item) => item.id === category.id
+                )?.checked || false
+              }
+              handleChange={(e) => handleChange(e, "categories")}
+            />
+          ))}
+
+          {subcategories.map((subcategory) => (
+            <CheckBox
+              id={"subcategory" + subcategory.id}
+              key={"subcategory" + subcategory.id}
+              value={subcategory.id}
+              name={subcategory.subcategoryName}
+              label={subcategory.subcategoryName}
+              attribute="subcategory"
+              border={true}
+              checked={
+                checkboxStates?.subcategory?.find(
+                  (item) => item.id === subcategory.id
+                )?.checked || false
+              }
+              handleChange={(e) => handleChange(e, "subcategories")}
+            />
+          ))}
+
+          {products.map((product) => (
+            <CheckBox
+              id={"product" + product.id}
+              key={"product" + product.id}
+              value={product.id}
+              name={product.productName}
+              label={product.productName}
+              attribute="product"
+              border={true}
+              checked={
+                checkboxStates?.product?.find((item) => item.id === product.id)
+                  ?.checked || false
+              }
+              handleChange={(e) => handleChange(e, "products")}
+            />
+          ))}
+
+          {states.map((item) => (
+            <CheckBox
+              id={item}
+              value={item}
+              name={item}
+              label={
+                item == "forSale"
+                  ? "Pentru vânzare"
+                  : item == "inSale"
+                  ? "În vânzare"
+                  : item == "validated"
+                  ? "Validat"
+                  : "Nevalidat"
+              }
+              padding="5px"
+              borderRadius="5px"
+              width="150px"
+              border={true}
+              checked={status[item]}
+              handleChange={(e) => handleStatusChange(e, "status")}
+            />
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
