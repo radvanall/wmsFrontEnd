c1cc82c51dc3ca2605b8264fca2fb7a1a211b886
Radvan Alexandru <rip777ton@gmail.com>
2023-08-01 15:51:24 +0300

    Change the ItemTable's styles. Set the cells width to a width that has been calculated based on the number of columns.

--- src/components/ItemTable/ItemTable.js
+++ src/components/ItemTable/ItemTable.js
@@ -1,66 +1,120 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ItemTable.css";
 import { Link } from "react-router-dom";
 import BasicButton from "../BasicButton/BasicButton";
-const ItemTable = ({ data, header, handleFooterClick, children }) => {
-  let dataHeader = "";
-  if (data.length > 0) {
-    dataHeader = Object.keys(data[0]);
-  }
+import getState from "../../functions/getState";
+const ItemTable = ({
+  data,
+  header,
+  handleFooterClick,
+  handleEdit,
+  handleDelete,
+  handleDatails,
+  children,
+}) => {
+  // let dataHeader = "";
+  // if (data.length > 0) {
+  //   dataHeader = Object.keys(data[0]);
+  // }
+  const [dataHeader, setDataHeader] = useState([]);
+  const [cellWidth, setCellWidth] = useState(0);
+
+  useEffect(() => {
+    if (Array.isArray(data) && data.length > 0) {
+      setDataHeader(Object.keys(data[0]));
+      const columns =
+        handleEdit || handleDelete || handleDatails
+          ? Object.keys(data[0]).length
+          : Object.keys(data[0]).length - 1;
+      console.log("array lenght:", Object.keys(data[0]).length);
+      setCellWidth(90 / columns);
+    }
+  }, [data]);
 
   return (
     <div className="item__table__wrapper">
-      <h2>{header}</h2>
+      {/* <h2>{header}</h2> */}
       {children}
       {data.length > 0 ? (
-        <div className="item__table">
-          <table>
+        // <div className="item__table">
+        <>
+          <table className="item__table">
+            {header && <caption>{header}</caption>}
             <thead className="item__table__header">
               <tr>
                 {dataHeader.map((item) => (
-                  <th key={item} className={item}>
+                  <th
+                    key={item}
+                    className={item}
+                    style={
+                      item === "id"
+                        ? { width: "10%" }
+                        : { width: `${cellWidth}%` }
+                    }
+                  >
                     {item}
                   </th>
                 ))}
-                <th>Actiuni</th>
+                {handleDatails && <th>Actiuni</th>}
               </tr>
             </thead>
             <tbody className="item__table__body">
               {data.map((row) => (
                 <tr key={row.id}>
                   {Object.keys(row).map((key, index) => (
-                    <td key={index} className={key}>
-                      <div
-                        className={
-                          key !== "state"
-                            ? "cell"
-                            : row[key] === "in sale"
-                            ? "in__sale"
-                            : "in__waiting"
-                        }
-                      >
-                        {" "}
-                        {row[key]}
-                      </div>
+                    <td
+                      key={index}
+                      className={key}
+                      style={
+                        key === "id"
+                          ? { width: "10%" }
+                          : { width: `${cellWidth}%` }
+                      }
+                    >
+                      {key !== "Starea" ? (
+                        <div className="cell">{row[key]}</div>
+                      ) : (
+                        <div className="cell">{getState(row[key])}</div>
+                      )}
+                      {/* <div
+                      className={
+                        key !== "state"
+                          ? "cell"
+                          : row[key] === "in sale"
+                          ? "in__sale"
+                          : "in__waiting"
+                      }
+                    >
+                      {" "}
+                      {row[key]}
+                    </div> */}
                     </td>
                   ))}
-                  <td>
-                    <button className="item__table__details">Detalii</button>
-                  </td>
+                  {handleDatails && (
+                    <td>
+                      <button
+                        className="item__table__details"
+                        onClick={() => handleDatails(row.id)}
+                      >
+                        Detalii
+                      </button>
+                    </td>
+                  )}
                 </tr>
               ))}
             </tbody>
           </table>
-          <div className="all__item__table">
-            <BasicButton text="Vezi toate" handleClick={handleFooterClick} />
-            {/* <Link to={`/${itemLink}`} className="link__to__item">
-              Vezi toate
-            </Link> */}
-          </div>
-        </div>
+
+          {/* <Link to={`/${itemLink}`} className="link__to__item">
+      Vezi toate
+    </Link> */}
+        </>
       ) : (
         <h2>Nu exista rezultate</h2>
       )}
+      <div className="all__item__table">
+        <BasicButton text="Vezi toate" handleClick={handleFooterClick} />
+      </div>
     </div>
   );
 };
