0c274421ebbae5583c998f5f51f7420e06b2d249
Radvan Alexandru <rip777ton@gmail.com>
2023-08-02 18:14:42 +0300

    Add StockDataModal into the StockTable component. It displays the full data of the selected stock.

--- src/components/StockTable/StockTable.js
+++ src/components/StockTable/StockTable.js
@@ -4,6 +4,8 @@ import stocks from "../../stock";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import SingleProductMenu from "../../components/SingleProductMenu/SingleProductMenu";
+import StockDataModal from "../StockDataModal/StockDataModal";
+import { useToggle } from "../../hooks/useToggle";
 import {
   filterStocksByProductId,
   setNavigate,
@@ -13,6 +15,10 @@ const StockTable = ({ productId }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [tableData, setTableData] = useState([]);
+  const [currentStock, setCurrentStock] = useState(null);
+
+  const { status: isOpenStockData, toggleStatus: toggleStockData } =
+    useToggle(false);
   const { data, loading, error, fetchData } = useFetch(
     `http://localhost:8080/api/stock/getStocksByProductId/${productId}`
   );
@@ -29,22 +35,38 @@ const StockTable = ({ productId }) => {
     setTableData(newArray);
   };
   useEffect(() => {
-    getStocks();
+    if (data) getStocks();
   }, [data]);
   const navigateToStocks = () => {
     dispatch(setNavigate(true));
     dispatch(filterStocksByProductId(productId));
     navigate("/stocks");
   };
+  const handleDetails = (id) => {
+    const chosenStock = data.find((item) => parseInt(item.id) === parseInt(id));
+    setCurrentStock(chosenStock);
+    toggleStockData();
+  };
   return (
     <>
       {data && tableData && (
-        <ItemTable
-          data={tableData}
-          header={"stocuri:"}
-          // itemLink={"operatorsinvoice"}
-          handleFooterClick={navigateToStocks}
-        />
+        <>
+          <ItemTable
+            data={tableData}
+            header={"stocuri:"}
+            // itemLink={"operatorsinvoice"}
+            handleDatails={handleDetails}
+            handleFooterClick={navigateToStocks}
+          />
+          {currentStock && (
+            <StockDataModal
+              active={isOpenStockData}
+              handleCloseModal={toggleStockData}
+              stock={currentStock}
+              refetchPage={fetchData}
+            />
+          )}
+        </>
       )}
     </>
   );
