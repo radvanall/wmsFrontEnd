e32764e44200dd3732b017c96d7d147d621f439f
Radvan Alexandru <rip777ton@gmail.com>
2023-08-03 18:45:59 +0300

    Remove unused code

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 const useGetData = (url) => {
@@ -8,40 +8,14 @@ const useGetData = (url) => {
   const getData = async (id) => {
     setLoading(true);
     try {
-      const fetch = await axios.get(
-        url + `${id}`
-        //   , {
-        //     headers: {
-        //       Accept: "application/json",
-        //     },
-        // }
-      );
+      const fetch = await axios.get(url + `${id}`);
       console.log("data is: ", fetch);
       setData(fetch.data);
-      //const product = response.data;
     } catch (err) {
       setError(err.message);
     } finally {
       setLoading(false);
     }
-
-    // axios
-    //   .get(url + `${id}`)
-    //   .then((response) => {
-    //     const product = response.data; // extract the person object from the response data
-    //     const imageUrl = URL.createObjectURL(
-    //       new Blob([product.image], { type: "image/jpeg" })
-    //     ); // create object URL from binary image data
-    //     // do something with the person object and image URL (e.g. display them on the page)
-    //     setData(product);
-    //     setImg(imageUrl);
-    //     setLoading(false);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     setLoading(false);
-    //   })
-    //   .finally(setLoading(false));
   };
   return { data, loading, error, getData };
 };
