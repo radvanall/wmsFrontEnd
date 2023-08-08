614411d9d4bc5a9206d94a2e5d0847d44390461f
Radvan Alexandru <rip777ton@gmail.com>
2023-08-08 17:34:52 +0300

    Remove unused code

--- src/components/CustomSelect/CustomSelect.js
+++ src/components/CustomSelect/CustomSelect.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
-import { useState } from "react";
 import "./CustomSelect.css";
-import zIndex from "@mui/material/styles/zIndex";
+
 const CustomSelect = ({
   positions,
   handleSelect,
@@ -15,41 +14,17 @@ const CustomSelect = ({
   readOnly,
   zIndex,
 }) => {
-  // const [opened, setOpened] = useState(false);
-  // const [selected, setSelected] = useState("");
-  // const [image, setImage] = useState("/img/57x57.png");
-  // const handleClick = (event) => {
-  //   const element = positions.find(
-  //     (obj) => parseInt(obj.id, 10) === parseInt(event.target.id, 10)
-  //   );
-  //   console.log(element);
-  //   setSelected(element.name);
-  //   setOpened((prev) => !prev);
-  //   setImage(element.image);
-  //   setFormFields((prev) => ({
-  //     ...prev,
-  //     product: event.target.id,
-  //   }));
-  // };
-  // const handleChange = () => {
-  //   console.log("position lenght=", positions.length);
-  //   if (positions.length > 0) alert("datele introduse vor fi pierdute");
-  // };
-
   return (
     <div>
       <div className="custom_select_selected">
-        {/* <img src="/img/57x57.png" /> */}
         {image && <img src={image} />}
         <input
           type="text"
           className="select_input"
-          // readOnly
           readOnly={readOnly || disableSelect}
           value={selected}
           onChange={handleChange}
         />
-
         <button
           className={disableSelect ? "disabled" : "select_button"}
           onClick={(e) => {
@@ -65,7 +40,7 @@ const CustomSelect = ({
 
       <ul
         className={opened ? "select_list" : "select_list hidden"}
-        style={zIndex ? { zIndex: zIndex } : ""}
+        style={zIndex ? { zIndex: zIndex } : { zIndex: 3 }}
       >
         {positions &&
           positions.map((item) => (
