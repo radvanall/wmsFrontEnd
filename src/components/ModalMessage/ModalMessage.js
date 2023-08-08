258cfc9ac006e0b21e0f026b6c7c74aef04ed7af
Radvan Alexandru <rip777ton@gmail.com>
2023-08-08 17:36:39 +0300

    Add styles for ModalMessage

--- src/components/ModalMessage/ModalMessage.js
+++ src/components/ModalMessage/ModalMessage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Modal from "../Modal/Modal";
 import CloseModal from "../CloseModal/CloseModal";
 import BasicButton from "../BasicButton/BasicButton";
+import "./ModalMessage.css";
 const ModalMessage = ({
   isOpened,
   close,
@@ -13,11 +14,13 @@ const ModalMessage = ({
   return (
     <Modal active={isOpened}>
       <CloseModal handleCloseModal={close} />
-      <p>{text && text}</p>
-      <div>{children && children}</div>
-      <div>
-        <BasicButton text="Confirmă" handleClick={handleOk} />
-        <BasicButton text="Anulează" handleClick={handleCancel} />
+      {text && <p className="modal__text">{text}</p>}
+      {children && <div className="modal__children">{children}</div>}
+      <div className="modal__message__buttons">
+        {handleOk && <BasicButton text="Confirmă" handleClick={handleOk} />}
+        {handleCancel && (
+          <BasicButton text="Anulează" handleClick={handleCancel} />
+        )}
       </div>
     </Modal>
   );
