eb2b8c0259eee812472efddcf2a9eea68233b3c4
Radvan Alexandru <rip777ton@gmail.com>
2023-08-10 18:54:16 +0300

    Add the address field and the edit Modal for this field. Add styles for edit field

--- src/components/InvoiceCard/InvoiceCard.js
+++ src/components/InvoiceCard/InvoiceCard.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./InvoiceCard.css";
 import Card from "../Card/Card";
+import { TiEdit } from "react-icons/ti";
 import getFormatedDate from "../../functions/getFormatedDate";
-const InvoiceCard = ({ invoice }) => {
+import CardModal from "../CardModal/CardModal";
+import { useToggle } from "../../hooks/useToggle";
+const InvoiceCard = ({ invoice, refetch }) => {
   console.log("invoice:", invoice);
+  const { status: isOpenAddressModal, toggleStatus: toggleAddressModal } =
+    useToggle(false);
   return (
     <div className="single__order__card__wrapper">
       <Card>
@@ -26,12 +31,35 @@ const InvoiceCard = ({ invoice }) => {
               <span>Operatorul:</span>
               <span>{invoice.operatorName}</span>
             </p>
+            <div className="address__container">
+              <span>Adresa:</span>
+              <div>
+                {invoice.address === "inStore" ? "Pe loc" : invoice.address}
+                {/* Chisinau sarmisegetusa 23 ap 3 */}
+                <button
+                  className="address__button"
+                  onClick={() => {
+                    toggleAddressModal();
+                  }}
+                >
+                  <TiEdit className="edit__address__button menu__opened" />
+                </button>
+              </div>
+            </div>
             <p>
               <span>Prețul total:</span>
               <span>{invoice.totalPrice} lei</span>
             </p>
           </div>
         </div>
+        <CardModal
+          active={isOpenAddressModal}
+          handleCloseModal={toggleAddressModal}
+          clientAddress={invoice.clientAddress}
+          address={invoice.address}
+          refetch={refetch}
+          invoiceId={invoice.id}
+        />
       </Card>
     </div>
   );
