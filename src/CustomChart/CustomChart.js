c57053f48171a1eca1aad5575e1e871e8d7cb26d
Radvan Alexandru <rip777ton@gmail.com>
2023-08-14 20:00:41 +0300

    Add a div container to control the height of the chart

--- src/CustomChart/CustomChart.js
+++ src/CustomChart/CustomChart.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Chart } from "react-chartjs-2";
 
-const CustomChart = ({ chartData, options, plugins }) => {
-  return <Chart data={chartData} options={options} plugins={[plugins]}></Chart>;
+const CustomChart = ({ chartData, options, plugins, height }) => {
+  return (
+    <div style={{ height: height, position: "relative", paddingLeft: "5px" }}>
+      <Chart data={chartData} options={options} plugins={[plugins]}></Chart>
+    </div>
+  );
 };
 
 export default CustomChart;
