b6a1767a8286967f931c78d9112725467491ad66
Radvan Alexandru <rip777ton@gmail.com>
2023-08-14 20:09:46 +0300

    Add new components onto the page

--- src/pages/SingleCustomer/SingleCustomer.js
+++ src/pages/SingleCustomer/SingleCustomer.js
@@ -3,8 +3,14 @@ import { useParams } from "react-router-dom";
 import useGetData from "../../hooks/useGetData";
 import getFormatedDate from "../../functions/getFormatedDate";
 import CustomerCard from "../../components/CustomerCard/CustomerCard";
+import Card from "../../components/Card/Card";
+import CustomerPurchasesChart from "../../components/CustomersPurchasesChart/CustomerPurchasesChart";
+import FavoriteProducts from "../../components/FavoriteProducts/FavoriteProducts";
+import ResponsiveTable from "../../components/ResponsiveTable/ResponsiveTable";
 import "./SingleCustomer.css";
+import { useNavigate } from "react-router-dom";
 const SingleCustomer = () => {
+  const navigate = useNavigate();
   const { customerId } = useParams();
   const [invoices, setInvoices] = useState([]);
   const [customer, setCustomer] = useState({});
@@ -25,7 +31,27 @@ const SingleCustomer = () => {
         Adresa: invoice.address,
         Operatorul: invoice.operatorName,
         ["Preț total lei."]: invoice.totalPrice,
-        Validat: invoice.shipped,
+        Validat: invoice.shipped ? (
+          <div
+            style={{
+              backgroundColor: "#b4f8c8",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            Validat
+          </div>
+        ) : (
+          <div
+            style={{
+              backgroundColor: "#ffaebc",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            Nevalidat
+          </div>
+        ),
       }));
       setInvoices(newArray);
     }
@@ -33,7 +59,9 @@ const SingleCustomer = () => {
   useEffect(() => {
     getData(customerId);
   }, [customerId]);
-
+  const handleDetails = (id) => {
+    navigate(`/orders/${id}`);
+  };
   return (
     <div className="single__customer__wrapper">
       {data && (
@@ -43,11 +71,25 @@ const SingleCustomer = () => {
               <CustomerCard customer={customer} fetchData={fetchData} />{" "}
             </div>
           )}
+          <div className="lineChart">
+            <CustomerPurchasesChart id={customerId} />
+          </div>
+          <div className="invoices">
+            <Card>
+              {invoices.length && (
+                <ResponsiveTable
+                  data={invoices}
+                  title="Facturi"
+                  handleDetails={handleDetails}
+                />
+              )}{" "}
+            </Card>
+          </div>
+          <div className="products">
+            <FavoriteProducts id={customerId} />
+          </div>
         </>
       )}
-      <div className="lineChart">lineChart</div>
-      <div className="invoices">invoices</div>
-      <div className="products">products</div>
     </div>
   );
 };
