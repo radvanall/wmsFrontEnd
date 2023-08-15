756b47d451f8a2133277b401c8f1984242d495b4
Radvan Alexandru <rip777ton@gmail.com>
2023-08-15 19:32:11 +0300

    Add the chart and tables components on SinglePorvider page.

--- src/pages/SingleProvider/SingleProvider.js
+++ src/pages/SingleProvider/SingleProvider.js
@@ -1,9 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
+import ResponsiveTable from "../../components/ResponsiveTable/ResponsiveTable";
 import ProviderCard from "../../components/ProviderCard/ProviderCard";
 import { useParams } from "react-router-dom";
 import useGetData from "../../hooks/useGetData";
+import ProviderBalanceChart from "../../components/ProviderBalanceChart/ProviderBalanceChart";
+import "./SingleProvider.css";
+import { useNavigate } from "react-router-dom";
+import getFormatedDate from "../../functions/getFormatedDate";
 const SingleProvider = () => {
+  const navigate = useNavigate();
   const { providerId } = useParams();
+  const [products, setProducts] = useState([]);
+  const [invoices, setInvoices] = useState([]);
+  const [provider, setProvider] = useState({});
   const { data, loading, error, getData } = useGetData(
     "http://localhost:8080/api/provider/read/"
   );
@@ -13,13 +23,110 @@ const SingleProvider = () => {
     getData(providerId);
     console.log(data);
   }, []);
+  useEffect(() => {
+    if (data) {
+      const { positions, invoices, ...providerData } = data;
+      const invoicesArray = invoices.map((invoice) => ({
+        id: invoice.id,
+        Data: getFormatedDate(invoice.date, "RO-ro"),
+        ["Creată de"]: invoice.administrator,
+        ["Preț/ lei"]: invoice.price,
+        Validat: invoice.validated ? (
+          <div
+            style={{
+              backgroundColor: "#b4f8c8",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            Validat
+          </div>
+        ) : (
+          <div
+            style={{
+              backgroundColor: "#ffaebc",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            Nevalidat
+          </div>
+        ),
+      }));
+      const newArray = positions.map((position) => ({
+        id: position.id,
+        Nume: position.name,
+        image: position.image,
+        ["Cantitatea rămasă"]: position.remainingQuantity,
+        Starea: position.active ? (
+          <div
+            style={{
+              backgroundColor: "#b4f8c8",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            Activ
+          </div>
+        ) : (
+          <div
+            style={{
+              backgroundColor: "#ffaebc",
+              padding: "5px",
+              borderRadius: "5px",
+            }}
+          >
+            Inactiv
+          </div>
+        ),
+      }));
+      setProducts(newArray);
+      setProvider(providerData);
+      setInvoices(invoicesArray);
+    }
+  }, [data]);
+  const handleDetails = (id) => {
+    navigate(`/products/${id}`);
+  };
+  const handleInvoiceDetails = (id) => {
+    navigate(`/invoices/${id}`);
+  };
   return (
-    <div>
+    <div className="single__provider__wrapper">
       SingleProvider
       {data && (
-        <div style={{ maxWidth: 300 }}>
-          <ProviderCard provider={data} getData={getData} />
-        </div>
+        <>
+          {provider && (
+            <div className="card">
+              <ProviderCard provider={data} getData={getData} />
+            </div>
+          )}
+          <div className="lineChart">
+            <ProviderBalanceChart id={providerId} />
+          </div>
+          <div className="invoices">
+            <Card>
+              {invoices.length && (
+                <ResponsiveTable
+                  data={invoices}
+                  title="Facturi"
+                  handleDetails={handleInvoiceDetails}
+                />
+              )}
+            </Card>
+          </div>
+          <div className="products">
+            <Card>
+              {products.length && (
+                <ResponsiveTable
+                  data={products}
+                  title="Produsele furnizorului"
+                  handleDetails={handleDetails}
+                />
+              )}{" "}
+            </Card>
+          </div>
+        </>
       )}
     </div>
   );
