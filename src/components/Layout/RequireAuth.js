72772a4c2871b1b6ae714033ab80f850f8065865
Radvan Alexandru <rip777ton@gmail.com>
2023-08-19 20:07:37 +0300

    change the name of the role

--- src/components/Layout/RequireAuth.js
+++ src/components/Layout/RequireAuth.js
@@ -2,7 +2,7 @@ import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux/es/exports";
 
 const RequireAuth = ({ allowedRoles }) => {
-  const role = useSelector((state) => state.userSlice.userData?.role);
+  const role = useSelector((state) => state.userSlice.userData?.authority);
   const location = useLocation();
   return allowedRoles?.find((userRole) => userRole === role) ? (
     <Outlet />
