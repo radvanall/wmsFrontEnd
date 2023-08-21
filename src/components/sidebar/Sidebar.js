3b22e82d7318102e9f8f382dc1fd664511d1b96a
Radvan Alexandru <rip777ton@gmail.com>
2023-08-21 20:41:42 +0300

    Extract the data for links from Sidebar to links.js file. Render a list of links depending of the logged user's role.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,24 +1,33 @@
 import React from "react";
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
-import {
-  FaRegIdCard,
-  FaUserTie,
-  FaWarehouse,
-  FaShippingFast,
-  FaMoneyBill,
-  FaBox,
-  FaOutdent,
-  FaChartBar,
-  FaPager,
-  FaFileInvoice,
-} from "react-icons/fa";
+import { adminLinks, operatorLinks } from "../../links";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  console.log(adminLinks);
+  const authority = useSelector((state) => state.userSlice.userData?.authority);
   return (
     <div className="Sidebar">
       <ul>
-        <li>
+        {authority === "ROLE_ADMIN"
+          ? adminLinks.map((item) => (
+              <li key={item.text}>
+                <item.icon className="fa" />
+                <Link to={item.link} className="list">
+                  {item.text}
+                </Link>
+              </li>
+            ))
+          : operatorLinks.map((item) => (
+              <li key={item.text}>
+                <item.icon className="fa" />
+                <Link to={item.link} className="list">
+                  {item.text}
+                </Link>
+              </li>
+            ))}
+        {/* <li>
           <FaRegIdCard className="fa" />
           <Link to="/operators" className="list">
             Operatori
@@ -78,7 +87,7 @@ const Sidebar = () => {
           <Link to="/invoices" className="list">
             Facturi
           </Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
---
+++ src/links.js
@@ -0,0 +1,87 @@
+import {
+  FaRegIdCard,
+  FaUserTie,
+  FaWarehouse,
+  FaShippingFast,
+  FaMoneyBill,
+  FaBox,
+  FaOutdent,
+  FaChartBar,
+  FaPager,
+  FaFileInvoice,
+  FaHome,
+} from "react-icons/fa";
+export const adminLinks = [
+  {
+    icon: FaHome,
+    link: "/",
+    text: "Cabinet",
+  },
+  {
+    icon: FaRegIdCard,
+    link: "/operators",
+    text: "Operatori",
+  },
+  {
+    icon: FaUserTie,
+    link: "/customers",
+    text: "Clienți",
+  },
+  {
+    icon: FaWarehouse,
+    link: "/stocks",
+    text: "Stocuri",
+  },
+  {
+    icon: FaShippingFast,
+    link: "/providers",
+    text: "Furnizori",
+  },
+  {
+    icon: FaMoneyBill,
+    link: "/orders",
+    text: "Vânzări",
+  },
+  {
+    icon: FaBox,
+    link: "/products",
+    text: "Produse",
+  },
+  {
+    icon: FaChartBar,
+    link: "/stats",
+    text: "Statistica",
+  },
+  {
+    icon: FaFileInvoice,
+    link: "/invoices",
+    text: "Facturi",
+  },
+];
+export const operatorLinks = [
+  {
+    icon: FaUserTie,
+    link: "/customers",
+    text: "Clienți",
+  },
+  {
+    icon: FaWarehouse,
+    link: "/stocks",
+    text: "Stocuri",
+  },
+  {
+    icon: FaShippingFast,
+    link: "/providers",
+    text: "Furnizori",
+  },
+  {
+    icon: FaMoneyBill,
+    link: "/orders",
+    text: "Vânzări",
+  },
+  {
+    icon: FaBox,
+    link: "/products",
+    text: "Produse",
+  },
+];
