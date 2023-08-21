2fcc883f590bd1fba5cae177ca1748d25a7c5ad6
Radvan Alexandru <rip777ton@gmail.com>
2023-08-21 20:39:09 +0300

    Add the logout function to Logout link.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,8 +4,9 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { FaRegUser } from "react-icons/fa";
 import { Divide as Hamburger } from "hamburger-react";
-import { useDispatch, useSelector } from "react-redux";
 import { toggle as changeState } from "../../toolkitRedux/menuSlice";
+import { resetJwt, resetUserData } from "../../toolkitRedux/userSlice";
+import { useSelector, useDispatch } from "react-redux";
 const Navbar = () => {
   const dispatch = useDispatch();
   const [isOpen, setOpen] = useState(false);
@@ -29,28 +30,22 @@ const Navbar = () => {
       setHamburger();
     }
   }, [windowWidth]);
-
+  const logout = () => {
+    dispatch(resetJwt());
+    dispatch(resetUserData());
+  };
   return (
     <div className="Navbar">
       <div className="hamburger">
-        <Hamburger
-          toggled={isOpen}
-          toggle={() => setHamburger()}
-          // toggle={() => {
-          //   setOpen;
-          //   dispatch(changeState());
-          // }}
-          // toggle={setOpen}
-          // onClick={() => dispatch(changeState())}
-        ></Hamburger>
+        <Hamburger toggled={isOpen} toggle={() => setHamburger()}></Hamburger>
       </div>
       <div className="logo">Dashboard</div>
       <div className="login__container">
         <div className="avatar">
           <FaRegUser className="avatar_img"></FaRegUser>
         </div>
-        <Link to="/login" className="logo">
-          Login
+        <Link to="/login" className="logo" onClick={logout}>
+          Logout
         </Link>
       </div>
     </div>
