bce7b4e076d76e4600ce4fc3c3e7c1294b69c871
Radvan Alexandru <rip777ton@gmail.com>
2023-08-21 20:37:29 +0300

    Remove unused code.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,15 +5,19 @@ import { useSelector } from "react-redux";
 import Sidebar from "../sidebar/Sidebar";
 const Layout = () => {
   const opened = useSelector((state) => state.menuState.opened);
+  const jwt = useSelector((state) => state.userSlice.jwt);
   return (
     <div className="App">
+      {/* <Navigate to="/home" /> */}
       <div className={opened ? "page_opened" : "page_closed"}></div>
       <div className="_container">
-        <Navbar />
+        {jwt !== "" && jwt !== null && <Navbar />}
         <div className="content">
-          <div className={opened ? " sidebar_opened" : "sidebar"}>
-            <Sidebar />
-          </div>
+          {jwt !== "" && jwt !== null && (
+            <div className={opened ? " sidebar_opened" : "sidebar"}>
+              <Sidebar />
+            </div>
+          )}
           <div className="page">
             <Outlet />
           </div>
