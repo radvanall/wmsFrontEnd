69f0cdd705c5aa2f5783bebc8ab4e7dabf9e63f3
Radvan Alexandru <rip777ton@gmail.com>
2023-08-22 19:47:59 +0300

    Add a width props to component to control it's width.

--- src/components/AdminPageTable/AdminPageTable.js
+++ src/components/AdminPageTable/AdminPageTable.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import ResponsiveTable from "../ResponsiveTable/ResponsiveTable";
 import { useNavigate } from "react-router-dom";
 import useGetData from "../../hooks/useGetData";
-const AdminPageTable = ({ getId, url, title, getFields, navTo }) => {
+const AdminPageTable = ({ getId, url, title, getFields, navTo, width }) => {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
   const { data, loading, error, getData } = useGetData(url);
@@ -11,6 +11,7 @@ const AdminPageTable = ({ getId, url, title, getFields, navTo }) => {
   }, []);
   useEffect(() => {
     if (data) {
+      console.log("admintable:", data);
       const newProducts = data.map((item) => getFields(item));
       setProducts(newProducts);
     }
@@ -19,7 +20,10 @@ const AdminPageTable = ({ getId, url, title, getFields, navTo }) => {
     navigate(`/${navTo}/${id}`);
   };
   return (
-    <div className="operator__invoice__wrapper">
+    <div
+      className="operator__invoice__wrapper"
+      style={{ width: width ? width : "45%" }}
+    >
       {products.length && (
         <ResponsiveTable
           data={products}
