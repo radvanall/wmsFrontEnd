edaeb4968e05c37053d678f0673560fdf8592221
Radvan Alexandru <rip777ton@gmail.com>
2023-08-26 21:15:34 +0300

    Substring first 10 characters of the ISOString and create the dates.

--- src/functions/areDatesEquals.js
+++ src/functions/areDatesEquals.js
@@ -1,10 +1,19 @@
 const areDateEquals = (firstDateString, secondDateString) => {
-  const firstDate = new Date(firstDateString);
-  const secondDate = new Date(secondDateString);
+  const firstDate = new Date(firstDateString.substring(0, 10));
+  const secondDate = new Date(secondDateString.substring(0, 10));
 
+  console.log(firstDate);
+  console.log(secondDate);
+
+  console.log(
+    firstDate.getFullYear() === secondDate.getFullYear() &&
+      firstDate.getMonth() === secondDate.getMonth() &&
+      firstDate.getDate() === secondDate.getDate()
+  );
   return (
     firstDate.getFullYear() === secondDate.getFullYear() &&
     firstDate.getMonth() === secondDate.getMonth() &&
     firstDate.getDate() === secondDate.getDate()
   );
 };
+export default areDateEquals;
