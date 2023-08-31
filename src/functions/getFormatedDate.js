393e5fa5fc158b8f17f66ad80d59e8d53c959b83
Radvan Alexandru <rip777ton@gmail.com>
2023-08-31 19:15:37 +0300

    Change toUpperCase to toLowerCase

--- src/functions/getFormatedDate.js
+++ src/functions/getFormatedDate.js
@@ -1,6 +1,6 @@
 const getFormatedDate = (date, locale) => {
   const options = { day: "numeric", month: "long", year: "numeric" };
   if (!date) return "--";
-  return new Date(date).toLocaleDateString(locale, options).toUpperCase();
+  return new Date(date).toLocaleDateString(locale, options).toLowerCase();
 };
 export default getFormatedDate;
