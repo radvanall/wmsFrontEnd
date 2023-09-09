6c55248e76f57d2032c5cafd5d5ea13e985e4a77
Radvan Alexandru <rip777ton@gmail.com>
2023-09-09 19:31:00 +0300

    Remove unused code.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,23 +1,11 @@
 import "./Table.css";
 import { Link } from "react-router-dom";
 import React from "react";
-import { useEffect, useState, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import Pagination from "../Pagination/Pagination";
 import { FaSortNumericDown, FaSortNumericUp } from "react-icons/fa";
 import useTableSettings from "./useTableSettings";
 
-// export default function Table({ info }) {
 export default function Table({ data, page, coloredCell }) {
-  // const info = useSelector((state) => state.productsSlice.displaiedData);
-  // const data2 = useSelector((state) => state.productsSlice.data);
-  // const [info, setInfo] = useState(data);
-  // useEffect(() => {
-  //   setInfo([...data]);
-  // }, [data]);
-  // console.log("infortdata=", data);
-  // console.log("infort=", info);
-
   const {
     tableHeader,
     sortColumnsState,
@@ -31,9 +19,6 @@ export default function Table({ data, page, coloredCell }) {
 
   return (
     <div className="table__wrapper basic__table">
-      {/* {data.length === 0 ? (
-        <h2>Nu exista rezultate</h2>
-      ) : ( */}
       <div>
         <table>
           <thead>
@@ -62,17 +47,13 @@ export default function Table({ data, page, coloredCell }) {
                 {Object.keys(item).map((key, index) => (
                   <td key={index} className={key}>
                     <div
-                      // className={`${key} ${
-                      //   key === "status" ? item[key] : "cell"
-                      // } container`}
                       className={`${key} ${
                         key === coloredCell ? item[key].state : "cell"
                       } container`}
                     >
                       {key === "img" ? (
                         <img src={item[key]} alt="" />
-                      ) : // item[key]
-                      key === coloredCell ? (
+                      ) : key === coloredCell ? (
                         item[key].text
                       ) : (
                         item[key]
