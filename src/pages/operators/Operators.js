6b88aaa828cba217bd718b5bc585a74d367e14a9
Radvan Alexandru <rip777ton@gmail.com>
2023-09-16 17:45:21 +0300

    reomve unused code.

--- src/pages/operators/Operators.js
+++ src/pages/operators/Operators.js
@@ -5,15 +5,7 @@ import Table from "../../components/Table/Table";
 import useFetch from "../../hooks/useFetch";
 import OperatorMenu from "../../components/OperatorMenu/OperatorMenu";
 import DeleteMessage from "../../components/DeleteMessage/DeleteMessage";
-//import data from "../../data";
 const Operators = () => {
-  // const [data, setData] = useState([]);
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/api/operator/readOperatorsTable")
-  //     .then((response) => response.json())
-  //     .then((data) => setData(data));
-  // }, []);
-  // console.log("data=", data);
   const [operators, setOperators] = useState(null);
   const { data, loading, error, fetchData } = useFetch(
     "http://localhost:8080/api/operator/readOperatorsTable"
@@ -23,8 +15,6 @@ const Operators = () => {
     if (data) {
       const newArray = data.map((operator) => {
         return {
-          // Validat: invoice.validated ? "validat" : "nevalidat",
-          // ...operator,
           id: operator.id,
           img: operator.img,
           Nickname: operator.nickname,
