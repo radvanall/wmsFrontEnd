8bc996a45e8ced99929db2dd84c365248e2ab3ac
Radvan Alexandru <rip777ton@gmail.com>
2023-09-19 16:27:19 +0300

    Remove unused code.

--- src/components/productsMenu/ProductsMenu.js
+++ src/components/productsMenu/ProductsMenu.js
@@ -3,33 +3,16 @@ import "./ProductsMenu.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { BiSearchAlt } from "react-icons/bi";
 import { FiFilter } from "react-icons/fi";
-import { MdRefresh } from "react-icons/md";
 import { BiPlusMedical } from "react-icons/bi";
-import { useState } from "react";
 import FilterBar from "../FilterBar/FilterBar";
 import CreateProduct from "../CreateProduct/CreateProduct";
 import { useToggle } from "../../hooks/useToggle";
 import DeleteMessage from "../DeleteMessage/DeleteMessage";
 
-// const SearchMenu = ({ info, changeProducts }) => {
 const ProductsMenu = () => {
   const { status: isOpenSearch, toggleStatus: toggleSearch } = useToggle(false);
   const { status: isOpenFilter, toggleStatus: toggleFilter } = useToggle(false);
   const { status: isOpenCreate, toggleStatus: toggleCreate } = useToggle(false);
-
-  // const [isOpenSearch, setIsOpenSearch] = useState(false);
-  // const [isOpenFilter, setIsOpenFilter] = useState(false);
-  // const [isOpenCreate, setIsOpenCreate] = useState(false);
-
-  // const toggleSearch = () => {
-  //   setIsOpenSearch(!isOpenSearch);
-  // };
-  // const toggleFilter = () => {
-  //   setIsOpenFilter(!isOpenFilter);
-  // };
-  // const toggleCreate = () => {
-  //   setIsOpenCreate(!isOpenCreate);
-  // };
   return (
     <div>
       <div className="menu__buttons">
@@ -58,7 +41,6 @@ const ProductsMenu = () => {
           onClick={toggleFilter}
         />
       </div>{" "}
-      {/* <SearchBar info={info} changeProducts={changeProducts} /> */}
       <SearchBar isOpenSearch={isOpenSearch} />
       <FilterBar isOpenFilter={isOpenFilter} />
       <CreateProduct active={isOpenCreate} setActive={toggleCreate} />
