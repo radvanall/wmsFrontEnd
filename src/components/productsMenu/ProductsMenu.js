093c03b11f858579e068a36eb82334abc161794d
Radvan Alexandru <rip777ton@gmail.com>
2023-09-22 18:33:53 +0300

    Add CreateCategory modal.

--- src/components/productsMenu/ProductsMenu.js
+++ src/components/productsMenu/ProductsMenu.js
@@ -8,22 +8,60 @@ import FilterBar from "../FilterBar/FilterBar";
 import CreateProduct from "../CreateProduct/CreateProduct";
 import { useToggle } from "../../hooks/useToggle";
 import DeleteMessage from "../DeleteMessage/DeleteMessage";
+import CreateCategory from "../CreateCategory/CreateCategory";
 
 const ProductsMenu = () => {
   const { status: isOpenSearch, toggleStatus: toggleSearch } = useToggle(false);
   const { status: isOpenFilter, toggleStatus: toggleFilter } = useToggle(false);
   const { status: isOpenCreate, toggleStatus: toggleCreate } = useToggle(false);
+  const { status: isOpenProductCreate, toggleStatus: toggleProductCreate } =
+    useToggle(false);
+  const { status: isOpenCategoryCreate, toggleStatus: toggleCategoryCreate } =
+    useToggle(false);
+  const {
+    status: isOpenSubcategoryCreate,
+    toggleStatus: toggleSubcategoryCreate,
+  } = useToggle(false);
+
   return (
     <div>
       <div className="menu__buttons">
-        <BiPlusMedical
-          className={
-            isOpenCreate
-              ? "search_menu_button menu__opened"
-              : "search_menu_button menu__closed"
-          }
-          onClick={toggleCreate}
-        />
+        <div className="create__popup">
+          <BiPlusMedical
+            className={
+              isOpenCreate
+                ? "search_menu_button menu__opened"
+                : "search_menu_button menu__closed"
+            }
+            onClick={toggleCreate}
+          />
+          <ul className={isOpenCreate ? "popup__opened" : "popup__closed"}>
+            <li
+              onClick={() => {
+                toggleProductCreate();
+                toggleCreate();
+              }}
+            >
+              Crează produs
+            </li>
+            <li
+              onClick={() => {
+                toggleCategoryCreate();
+                toggleCreate();
+              }}
+            >
+              Crează categorie
+            </li>
+            <li
+              onClick={() => {
+                toggleSubcategoryCreate();
+                toggleCreate();
+              }}
+            >
+              Crează subcategorie
+            </li>
+          </ul>
+        </div>
         <BiSearchAlt
           className={
             isOpenSearch
@@ -43,7 +81,22 @@ const ProductsMenu = () => {
       </div>{" "}
       <SearchBar isOpenSearch={isOpenSearch} />
       <FilterBar isOpenFilter={isOpenFilter} />
-      <CreateProduct active={isOpenCreate} setActive={toggleCreate} />
+      <CreateProduct
+        active={isOpenProductCreate}
+        setActive={toggleProductCreate}
+      />
+      <CreateCategory
+        label="Introduceți categoria nouă"
+        active={isOpenCategoryCreate}
+        close={toggleCategoryCreate}
+        endpoint="http://localhost:8080/api/category/create"
+      />
+      <CreateCategory
+        label="Introduceți subcategoria nouă"
+        active={isOpenSubcategoryCreate}
+        close={toggleSubcategoryCreate}
+        endpoint="http://localhost:8080/api/subcategory/create"
+      />
       <DeleteMessage />
     </div>
   );
