9ef34e475cf25b4e0c57a87f4db6c1b472047ad2
Radvan Alexandru <rip777ton@gmail.com>
2023-10-08 21:57:19 +0300

    Add the jw token to put request

--- src/components/ProductForm/ProductFormFunctions/putProduct.js
+++ src/components/ProductForm/ProductFormFunctions/putProduct.js
@@ -3,13 +3,16 @@ const putProduct = (
   dataEntities,
   setServerResponse,
   dispatch,
-  fetchProducts
+  fetchProducts,
+  jwt
 ) => {
   axios
-    .put("http://localhost:8080/api/position/update", dataEntities)
+    .put("http://localhost:8080/api/position/update", dataEntities, {
+      headers: { Authorization: `Bearer ${jwt}` },
+    })
     .then((response) => {
       console.log(response);
-      dispatch(fetchProducts());
+      dispatch(fetchProducts(jwt));
       setServerResponse(response.data);
     })
     .catch((error) => setServerResponse(error.message));
