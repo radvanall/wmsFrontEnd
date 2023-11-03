7b9cf29bce16bb527bfb0f7bf61f8eaa45694e4b
Radvan Alexandru <rip777ton@gmail.com>
2023-11-03 16:32:56 +0200

    Add CloseModal button.

--- src/components/ProviderModal/ProviderModal.js
+++ src/components/ProviderModal/ProviderModal.js
@@ -2,6 +2,7 @@ import React from "react";
 import { CgCloseR } from "react-icons/cg";
 import Modal from "../Modal/Modal";
 import { MdOutlineOpenInBrowser } from "react-icons/md";
+import CloseModal from "../CloseModal/CloseModal";
 import ImagePicker from "../ImagePicker/ImagePicker";
 import "./ProviderModal.css";
 const ProviderModal = ({
@@ -19,45 +20,53 @@ const ProviderModal = ({
 }) => {
   return (
     <Modal active={active}>
-      <CgCloseR onClick={handleCloseForm} />
+      <CloseModal handleCloseModal={handleCloseForm} />
       <h2 className="form__title">{title}</h2>
       <div className="provider__form__wrapper">
         <form className="provider__form" onSubmit={handleSubmit} ref={formRef}>
-          <label htmlFor="providerNameId">Numele furnizorului:</label>
-          <input
-            type="text"
-            name="providerName"
-            id="providerNameId"
-            value={formFields.providerName}
-            onChange={handleFormChange}
-          />
-          {nameError && (
-            <label style={{ color: "red" }}>Numele e obligatoriu!</label>
-          )}
-          <label htmlFor="providerAddressId">Adresa furnizorului:</label>
-          <input
-            type="text"
-            name="address"
-            id="providerAddressId"
-            value={formFields.address}
-            onChange={handleFormChange}
-          />
-          <label htmlFor="providerEmailId">Email-ul furnizorului:</label>
-          <input
-            type="text"
-            name="email"
-            id="providerEmailId"
-            value={formFields.email}
-            onChange={handleFormChange}
-          />
-          <label htmlFor="providerTelId">Telefonul furnizorului:</label>
-          <input
-            type="text"
-            name="tel"
-            id="providerTelId"
-            value={formFields.tel}
-            onChange={handleFormChange}
-          />
+          <div className="input__container">
+            <label htmlFor="providerNameId">Numele furnizorului:</label>
+            <input
+              type="text"
+              name="providerName"
+              id="providerNameId"
+              value={formFields.providerName}
+              onChange={handleFormChange}
+            />
+            {nameError && (
+              <label style={{ color: "red" }}>Numele e obligatoriu!</label>
+            )}
+          </div>
+          <div className="input__container">
+            <label htmlFor="providerAddressId">Adresa furnizorului:</label>
+            <input
+              type="text"
+              name="address"
+              id="providerAddressId"
+              value={formFields.address}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="input__container">
+            <label htmlFor="providerEmailId">Email-ul furnizorului:</label>
+            <input
+              type="email"
+              name="email"
+              id="providerEmailId"
+              value={formFields.email}
+              onChange={handleFormChange}
+            />
+          </div>
+          <div className="input__container">
+            <label htmlFor="providerTelId">Telefonul furnizorului:</label>
+            <input
+              type="text"
+              name="tel"
+              id="providerTelId"
+              value={formFields.tel}
+              onChange={handleFormChange}
+            />
+          </div>
           <label className="img_label">
             Alegeți imaginea:
             <ImagePicker
@@ -88,8 +97,9 @@ const ProviderModal = ({
           <button type="submit" className="submit_product_buton">
             Submit
           </button>
+          {message && <label style={{ color: "red" }}>{message}</label>}
         </form>
-        {message && <label style={{ color: "red" }}>{message}</label>}
+
         <img src={image} alt="img" className="product_add_img" />
       </div>
     </Modal>
